29fc991b28951732ed5a7f8d8b613226563cae08
Paul Buonopane <techsoftadvanced@gmail.com>
2012-02-24 11:33:34 -0500

    Partial commit.  Worked on dividing Essentials into components:  GroupsComponent

--- Essentials/src/com/earth2me/essentials/settings/GroupsComponent.java
+++ Essentials/src/com/earth2me/essentials/settings/GroupsComponent.java
@@ -22,6 +22,22 @@ public class GroupsComponent extends AsyncStorageObjectHolder<Groups> implements
 	public GroupsComponent(final IContext context)
 	{
 		super(context, Groups.class);
+	}
+
+	@Override
+	public String getTypeId()
+	{
+		return "GroupsComponent";
+	}
+
+	@Override
+	public void initialize()
+	{
+	}
+
+	@Override
+	public void onEnable()
+	{
 		reload();
 	}
 
@@ -30,7 +46,7 @@ public class GroupsComponent extends AsyncStorageObjectHolder<Groups> implements
 	{
 		return new File(context.getDataFolder(), "groups.yml");
 	}
-	
+
 	public Collection<Entry<String, GroupOptions>> getGroups(final IUser player)
 	{
 		acquireReadLock();
@@ -41,7 +57,7 @@ public class GroupsComponent extends AsyncStorageObjectHolder<Groups> implements
 			{
 				return Collections.emptyList();
 			}
-			final ArrayList<Entry<String, GroupOptions>> list = new ArrayList();
+			final ArrayList<Entry<String, GroupOptions>> list = new ArrayList<Entry<String, GroupOptions>>();
 			for (Entry<String, GroupOptions> entry : groups.entrySet())
 			{
 				if (GroupsPermissions.getPermission(entry.getKey()).isAuthorized(player))
@@ -85,7 +101,7 @@ public class GroupsComponent extends AsyncStorageObjectHolder<Groups> implements
 		}
 		return 0;
 	}
-	
+
 	@Override
 	public double getTeleportDelay(final IUser player)
 	{
@@ -137,7 +153,7 @@ public class GroupsComponent extends AsyncStorageObjectHolder<Groups> implements
 		}
 		return 0;
 	}
-	
+
 	//TODO: Reimplement caching
 	@Override
 	public MessageFormat getChatFormat(final IUser player)
@@ -151,9 +167,9 @@ public class GroupsComponent extends AsyncStorageObjectHolder<Groups> implements
 			format = format.replace("{SHORTWORLDNAME}", "{2}");
 			format = format.replaceAll("\\{(\\D*)\\}", "\\[$1\\]");
 			MessageFormat mFormat = new MessageFormat(format);
-			return mFormat;		
+			return mFormat;
 	}
-	
+
 	private String getRawChatFormat(final IUser player)
 	{
 		for (Entry<String, GroupOptions> groupOptions : getGroups(player))
@@ -191,5 +207,5 @@ public class GroupsComponent extends AsyncStorageObjectHolder<Groups> implements
 		}
 		return "default";
 	}
-	
+
 }
