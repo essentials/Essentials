e1eed9a91253bf1f9e0f3bc894660add7b365173
BranicYeti <BranicYeti@live.co.uk>
2012-06-24 00:57:59 +0100

    Clearing inventory now clear the armour slots too
    /ci clears armour slots too. Will sort out arguments another time

--- Essentials/src/com/earth2me/essentials/commands/Commandclearinventory.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandclearinventory.java
@@ -3,9 +3,11 @@ package com.earth2me.essentials.commands;
 import static com.earth2me.essentials.I18n._;
 import com.earth2me.essentials.User;
 import java.util.List;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 
 public class Commandclearinventory extends EssentialsCommand
@@ -30,7 +32,12 @@ public class Commandclearinventory extends EssentialsCommand
 				{
 					for (Player p : online)
 					{
+						ItemStack clear = new ItemStack(Material.AIR, 1);
 						p.getInventory().clear();
+						p.getInventory().setBoots(clear);
+						p.getInventory().setChestplate(clear);
+						p.getInventory().setHelmet(clear);
+						p.getInventory().setLeggings(clear);
 						user.sendMessage(_("inventoryClearedOthers", p.getDisplayName()));
 					}
 					return;
@@ -43,6 +50,10 @@ public class Commandclearinventory extends EssentialsCommand
 				if (p != null)
 				{
 					p.getInventory().clear();
+					p.getInventory().setBoots(null);
+					p.getInventory().setChestplate(null);
+					p.getInventory().setHelmet(null);
+					p.getInventory().setLeggings(null);
 					user.sendMessage(_("inventoryClearedOthers", p.getDisplayName()));
 				}
 				else
@@ -54,6 +65,10 @@ public class Commandclearinventory extends EssentialsCommand
 		else
 		{
 			user.getInventory().clear();
+			user.getInventory().setBoots(null);
+			user.getInventory().setChestplate(null);
+			user.getInventory().setHelmet(null);
+			user.getInventory().setLeggings(null);
 			user.sendMessage(_("inventoryCleared"));
 		}
 	}
@@ -75,6 +90,10 @@ public class Commandclearinventory extends EssentialsCommand
 				for (Player p : online)
 				{
 					p.getInventory().clear();
+					p.getInventory().setBoots(null);
+					p.getInventory().setChestplate(null);
+					p.getInventory().setHelmet(null);
+					p.getInventory().setLeggings(null);
 					sender.sendMessage(_("inventoryClearedOthers", p.getDisplayName()));
 				}
 				return;
@@ -87,6 +106,10 @@ public class Commandclearinventory extends EssentialsCommand
 			if (u != null)
 			{
 				u.getInventory().clear();
+				u.getInventory().setBoots(null);
+				u.getInventory().setChestplate(null);
+				u.getInventory().setHelmet(null);
+				u.getInventory().setLeggings(null);
 				sender.sendMessage(_("inventoryClearedOthers", u.getDisplayName()));
 			}
 			else
