845fa008567974e51fb040b961163aa912965b27
ementalo <ementalodev@gmx.co.uk>
2012-06-26 13:21:48 +0100

    fix some encoding issues for §
    Don't attempt to close on null objects

--- Essentials/src/com/earth2me/essentials/utils/textreader/TextInput.java
+++ Essentials/src/com/earth2me/essentials/utils/textreader/TextInput.java
@@ -85,9 +85,9 @@ public class TextInput implements IText
 						if (line.length() > 0 && line.charAt(0) == '#')
 						{
 							bookmarks.put(line.substring(1).toLowerCase(Locale.ENGLISH).replaceAll("&[0-9a-fk]", ""), lineNumber);
-							chapters.add(line.substring(1).replace('&', '�').replace("�", "&"));
+							chapters.add(line.substring(1).replace('&', '§').replace("§", "&"));
 						}
-						lines.add(line.replace('&', '�').replace("�", "&"));
+						lines.add(line.replace('&', '§').replace("§", "&"));
 						lineNumber++;
 					}
 				}
@@ -119,7 +119,9 @@ public class TextInput implements IText
 				}
 				finally
 				{
+                    if(output != null)
 					output.close();
+                    if(input != null)
 					input.close();
 				}
 				throw new FileNotFoundException("File " + filename + ".txt does not exist. Creating one for you.");
