7be9114d1aa90ac2fcae3352e08fc0a9c13b803d
md_5 <md_5@bigpond.com>
2012-10-07 21:26:56 +1100

    Helps to include the annotation

---
+++ EssentialsExtra/src/net/ess3/extra/AnnotatedCommand.java
@@ -0,0 +1,20 @@
+package net.ess3.extra;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AnnotatedCommand
+{
+	String description() default "";
+
+	String usage() default "";
+
+	String[] aliases() default 
+	{
+	};
+}
