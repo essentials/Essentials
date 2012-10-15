c74545d614cdb43611923ca153343976633adbe7
snowleo <schneeleo@gmail.com>
2012-10-15 23:12:02 +0200

    Add command option /more all

--- Essentials/src/net/ess3/commands/Commandmore.java
+++ Essentials/src/net/ess3/commands/Commandmore.java
@@ -13,34 +13,52 @@ public class Commandmore extends EssentialsCommand
 	@Override
 	public void run(final IUser user, final String commandLabel, final String[] args) throws Exception
 	{
-		final ItemStack stack = user.getPlayer().getItemInHand();
-		if (stack == null)
+		ItemStack[] stacks;
+		if (args.length > 0 && args[0].equalsIgnoreCase("all"))
 		{
-			throw new Exception(_("cantSpawnItem", "Air"));
-		}
-		ISettings settings = ess.getSettings();
-
-		int defaultStackSize = settings.getData().getGeneral().getDefaultStacksize();
-		int oversizedStackSize = settings.getData().getGeneral().getOversizedStacksize();
-
-		if (stack.getAmount() >= (Permissions.OVERSIZEDSTACKS.isAuthorized(user)
-								  ? oversizedStackSize
-								  : defaultStackSize > 0 ? defaultStackSize : stack.getMaxStackSize()))
-		{
-			throw new NoChargeException();
-		}
-		final String itemname = stack.getType().toString().toLowerCase(Locale.ENGLISH).replace("_", "");
-		if (!Permissions.ITEMSPAWN.isAuthorized(user, stack))
-		{
-			throw new Exception(_("cantSpawnItem", itemname));
-		}
-		if (Permissions.OVERSIZEDSTACKS.isAuthorized(user))
-		{
-			stack.setAmount(oversizedStackSize);
+			stacks = user.getPlayer().getInventory().getContents();
 		}
 		else
 		{
-			stack.setAmount(defaultStackSize > 0 ? defaultStackSize : stack.getMaxStackSize());
+			stacks = new ItemStack[]
+			{
+				user.getPlayer().getItemInHand()
+			};
+		}
+		for (ItemStack stack : stacks)
+		{
+			if (stack == null)
+			{
+				throw new Exception(_("cantSpawnItem", "Air"));
+			}
+			ISettings settings = ess.getSettings();
+			
+			int defaultStackSize = settings.getData().getGeneral().getDefaultStacksize();
+			int oversizedStackSize = settings.getData().getGeneral().getOversizedStacksize();
+			
+			if (stack.getAmount() >= (Permissions.OVERSIZEDSTACKS.isAuthorized(user)
+									  ? oversizedStackSize
+									  : defaultStackSize > 0 ? defaultStackSize : stack.getMaxStackSize()))
+			{
+				throw new NoChargeException();
+			}
+			final String itemname = stack.getType().toString().toLowerCase(Locale.ENGLISH).replace("_", "");
+			if (!Permissions.ITEMSPAWN.isAuthorized(user, stack))
+			{
+				throw new Exception(_("cantSpawnItem", itemname));
+			}
+			if (Permissions.OVERSIZEDSTACKS.isAuthorized(user))
+			{
+				stack.setAmount(oversizedStackSize);
+			}
+			else
+			{
+				stack.setAmount(defaultStackSize > 0 ? defaultStackSize : stack.getMaxStackSize());
+			}
+		}
+		if (stacks.length > 1)
+		{
+			user.getPlayer().getInventory().setContents(stacks);
 		}
 		user.getPlayer().updateInventory();
 	}
