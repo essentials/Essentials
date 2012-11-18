ba6dd779569d409544a0d774a98d7dd65ba4528e
Runemoro <runemoro@live.ca>
2012-11-17 20:14:11 -0500

    Add /pluginenable and /plugindisable commands

---
+++ Essentials/src/net/ess3/commands/Commandenableplugin.java
@@ -0,0 +1,29 @@
+package net.ess3.commands;
+
+import static net.ess3.I18n._;
+
+import net.ess3.api.IUser;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+
+
+public class Commandenableplugin extends EssentialsCommand
+{
+	@Override
+	public void run(final IUser user, final String commandLabel, final String[] args) throws Exception
+	{
+	    	if (args.length < 1) {
+	    	    throw new NotEnoughArgumentsException();
+	    	}
+	    	PluginManager pm = Bukkit.getServer().getPluginManager();
+	        Plugin plugin = pm.getPlugin(args[0]);
+	        if (plugin != null) {
+	            pm.enablePlugin(plugin);
+	            user.sendMessage(_("pluginEnabled"));
+	        } else {
+	            user.sendMessage(_("badPluginName")); 
+	        }	
+	}
+}
