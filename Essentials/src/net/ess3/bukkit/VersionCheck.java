b4ea7e66b1d3770dc76b8bdc67afabd90bd20328
Chris Ward <chris@chrisgward.com>
2013-01-09 00:06:12 +1100

    Force people to use latest CB version 3ea3d1892265f657d7325a33940e4677180825ba

--- Essentials/src/net/ess3/bukkit/VersionCheck.java
+++ Essentials/src/net/ess3/bukkit/VersionCheck.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.PluginManager;
 
 public class VersionCheck
 {
-	public static final int BUKKIT_VERSION = 2015;
+	public static final int BUKKIT_VERSION = 2543;
 	private static final Pattern bukkitVersionPattern = Pattern.compile(
 			"git-Bukkit-(?:(?:[0-9]+)\\.)+[0-9]+-R[\\.0-9]+-(?:[0-9]+-g[0-9a-f]+-)?b([0-9]+)jnks.*");
 
