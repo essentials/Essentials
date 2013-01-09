9222be9145c56c338c11203205f0a41f82d120e4
Chris Ward <chris@chrisgward.com>
2013-01-09 20:10:22 +1100

    Log target plugin in debug mode for altcmd 617959a11b23e0fe38d82be34247d71e9fcb525a

--- Essentials/src/net/ess3/commands/EssentialsCommandHandler.java
+++ Essentials/src/net/ess3/commands/EssentialsCommandHandler.java
@@ -61,7 +61,7 @@ public class EssentialsCommandHandler implements ICommandHandler, TabExecutor
 			if (pc != null)
 			{
 
-				executed(commandLabel, pc.getLabel());
+				executed(commandLabel, pc);
 				try
 				{
 					return pc.execute(sender, commandLabel, args);
@@ -193,7 +193,7 @@ public class EssentialsCommandHandler implements ICommandHandler, TabExecutor
 			if (pc != null)
 			{
 
-				executed(commandLabel, pc.getLabel());
+				executed(commandLabel, pc);
 				try
 				{
 					return pc.tabComplete(sender, commandLabel, args);
@@ -404,13 +404,14 @@ public class EssentialsCommandHandler implements ICommandHandler, TabExecutor
 		return pcommands.get(0);
 	}
 
-	public void executed(final String label, final String otherLabel)
+	public void executed(final String label, final PluginCommand pc)
 	{
+		final String altString = pc.getPlugin().getName() + ":" + pc.getLabel();
 		if (ess.getSettings().isDebug())
 		{
-			LOGGER.log(Level.INFO, "Essentials: Alternative command " + label + " found, using " + otherLabel);
+			LOGGER.log(Level.INFO, "Essentials: Alternative command " + label + " found, using " + altString);
 		}
-		disabledList.put(label, otherLabel);
+		disabledList.put(label, altString);
 	}
 
 	@Override
