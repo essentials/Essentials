05414f78d89e813ed946690b43fff684f8ce02a3
Necrodoom <doomed.war@gmail.com>
2013-02-28 15:06:47 +0200

    more derp

--- Essentials/src/com/earth2me/essentials/commands/Commandnick.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandnick.java
@@ -90,12 +90,12 @@ public class Commandnick extends EssentialsCommand
 
 	private void setNickname(final Server server, final User target, final String nick) throws Exception
 	{
-		final String usernick = User.getNickname();
+		final String targetnick = target.getNickname();
 		if (!nick.matches("^[a-zA-Z_0-9\u00a7]+$"))
 		{
 			throw new Exception(_("nickNamesAlpha"));
 		}
-		else if ("off".equalsIgnoreCase(nick) || (usernick != null && !usernick.isEmpty() &&  target.getName().equalsIgnoreCase(nick)))
+		else if ("off".equalsIgnoreCase(nick) || (targetnick != null && !targetnick.isEmpty() && target.getName().equalsIgnoreCase(nick)))
 		{
 			target.setNickname(null);
 			target.setDisplayNick();
