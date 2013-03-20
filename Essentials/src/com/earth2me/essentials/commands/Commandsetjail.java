5ba6d70df912dca14b4d321405ece00f5c86bb8c
Necrodoom <doomed.war@gmail.com>
2013-03-20 10:39:09 +0200

    [fix] deny empty jail name

--- Essentials/src/com/earth2me/essentials/commands/Commandsetjail.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandsetjail.java
@@ -20,8 +20,11 @@ public class Commandsetjail extends EssentialsCommand
 		{
 			throw new NotEnoughArgumentsException();
 		}
-		ess.getJails().setJail(args[0], user.getLocation());
-		user.sendMessage(_("jailSet", Util.sanitizeString(args[0])));
+		if (!args[0].isEmpty())
+		{
+			ess.getJails().setJail(args[0], user.getLocation());
+			user.sendMessage(_("jailSet", Util.sanitizeString(args[0])));
+		}
 
 	}
 }
