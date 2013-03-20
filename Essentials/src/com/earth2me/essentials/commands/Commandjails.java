67cdea9dd99b2595f478f628591dfab15674c03f
Necrodoom <doomed.war@gmail.com>
2013-03-20 10:29:47 +0200

    fix /jails to be up to par with /warp list

--- Essentials/src/com/earth2me/essentials/commands/Commandjails.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandjails.java
@@ -7,6 +7,9 @@ import org.bukkit.command.CommandSender;
 
 public class Commandjails extends EssentialsCommand
 {
+	private final static int JAILS_PER_PAGE = 20;
+
+	
 	public Commandjails()
 	{
 		super("jails");
@@ -15,6 +18,24 @@ public class Commandjails extends EssentialsCommand
 	@Override
 	protected void run(final Server server, final CommandSender sender, final String commandLabel, final String[] args) throws Exception
 	{
-		sender.sendMessage("§7" + Util.joinList(" ", ess.getJails().getList()));
+		final List<String> jailNameList = new ArrayList<String>(ess.getJails().getList()
+		final String jailList;
+		if (jailNameList.size() > JAILS_PER_PAGE)
+		{
+			int page = 1;
+			if (args.length > 0 && Util.isInt(args[0]))
+			{
+				page = Integer.parseInt(args[0]);
+			}
+			final int jailPage = (page - 1) * JAILS_PER_PAGE;
+			jailList = Util.joinList(jailNameList.subList(jailPage, jailPage + Math.min(jailNameList.size() - jailPage, JAILS_PER_PAGE)))
+			sender.sendMessage(_("jailsCount", jailNameList.size(), page, (int)Math.ceil(jailNameList.size() / (double)JAILS_PER_PAGE)));
+			sender.sendMessage(_("jailList", jailList));
+		}
+		else
+		{
+			jailList = Util.joinList(", ", jailNameList);
+			sender.sendMessage(_("jails", jailList));
+		}
 	}
 }
