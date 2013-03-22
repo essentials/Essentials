c47036258324aef0f809547af6f2ac03177db815
Necrodoom <doomed.war@gmail.com>
2013-03-22 12:05:01 +0200

    Update Commandafk.java

--- Essentials/src/com/earth2me/essentials/commands/Commandafk.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandafk.java
@@ -2,6 +2,7 @@ package com.earth2me.essentials.commands;
 
 import static com.earth2me.essentials.I18n._;
 import com.earth2me.essentials.User;
+import org.bukkit.command.CommandSender;
 import org.bukkit.Server;
 
 
