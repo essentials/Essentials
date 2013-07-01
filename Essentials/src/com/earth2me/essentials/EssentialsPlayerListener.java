2e20fbb5011af137af4b49bf964a86a8f904d9cb
dmulloy2 <dmulloy2@live.com>
2013-07-01 03:42:08 -0400

    Import chat color

--- Essentials/src/com/earth2me/essentials/EssentialsPlayerListener.java
+++ Essentials/src/com/earth2me/essentials/EssentialsPlayerListener.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
