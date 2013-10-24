844da20d4f053a59fb5932e80b241abcf95a7804
Dabo Ross <daboross@daboross.net>
2013-10-24 04:53:28 -0700

    Fix JavaDoc formatting

--- EssentialsSpawn/src/com/earth2me/essentials/spawn/IEssentialsSpawn.java
+++ EssentialsSpawn/src/com/earth2me/essentials/spawn/IEssentialsSpawn.java
@@ -9,6 +9,7 @@ public interface IEssentialsSpawn extends Plugin
 
 	/**
 	 * Sets the spawn for a given group to a given location.
+	 *
 	 * @param loc The location to set the spawn to
 	 * @param group The group to set the spawn of, or 'default' for the default spawn
 	 * @throws IllegalArgumentException If group is null
@@ -17,6 +18,7 @@ public interface IEssentialsSpawn extends Plugin
 
 	/**
 	 * Gets the spawn location for a given group.
+	 *
 	 * @param group The group to get the spawn of, or 'default' for the default spawn
 	 * @return The spawn location set for the given group
 	 * @throws IllegalArgumentException If group is null
