f8e100f5b7c869b0cf1178efc849ee2138361b4b
Chris Ward <chris@chrisgward.com>
2013-12-01 17:04:42 +1100

    Trim long /gc output, add /gc all 63ce4a99b9

--- Essentials/src/net/ess3/commands/Commandgc.java
+++ Essentials/src/net/ess3/commands/Commandgc.java
@@ -1,11 +1,14 @@
 package net.ess3.commands;
 
-import java.lang.management.ManagementFactory;
-import static net.ess3.I18n._;
 import net.ess3.utils.DateUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+import static net.ess3.I18n._;
 
 
 public class Commandgc extends EssentialsCommand
@@ -35,25 +38,34 @@ public class Commandgc extends EssentialsCommand
 		sender.sendMessage(_("§6Free memory:§c {0} MB.", (runtime.freeMemory() / 1024 / 1024)));
 		sender.sendMessage(_("gcquene", (ess.getStorageQueue().getQueueSize())));
 
-		for (World w : server.getWorlds())
+		List<World> worlds = server.getWorlds();
+
+		if(worlds.size() > 3 && args.length == 0)
 		{
-			final String worldType;
-			switch (w.getEnvironment())
+			sender.sendMessage(_("§4Message truncated, to see the full output type:§c /{0} all", commandLabel));
+		}
+		else
+		{
+			for (World w : server.getWorlds())
 			{
-			case NETHER:
-				worldType = "Nether";
-				break;
-			case THE_END:
-				worldType = "The End";
-				break;
-			default:
-				worldType = "World";
-				break;
+				final String worldType;
+				switch (w.getEnvironment())
+				{
+				case NETHER:
+					worldType = "Nether";
+					break;
+				case THE_END:
+					worldType = "The End";
+					break;
+				default:
+					worldType = "World";
+					break;
 
+				}
+
+				sender.sendMessage(
+						worldType + " \"" + w.getName() + "\": " + w.getLoadedChunks().length + _("gcchunks") + w.getEntities().size() + _("gcentities"));
 			}
-
-			sender.sendMessage(
-					worldType + " \"" + w.getName() + "\": " + w.getLoadedChunks().length + _("gcchunks") + w.getEntities().size() + _("gcentities"));
 		}
 	}
 }
