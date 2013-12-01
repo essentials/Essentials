b42b5eadc9cc4495a1699c9982b4bd6b48e6e055
Chris Ward <chris@chrisgward.com>
2013-12-01 17:33:44 +1100

    Add optional 2nd arg to broadcast format. 50dca9abf54d5a2

--- Essentials/src/net/ess3/commands/Commandbroadcast.java
+++ Essentials/src/net/ess3/commands/Commandbroadcast.java
@@ -1,12 +1,26 @@
 package net.ess3.commands;
 
-import static net.ess3.I18n._;
+import net.ess3.api.IUser;
 import net.ess3.utils.FormatUtil;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static net.ess3.I18n._;
 
 
 public class Commandbroadcast extends EssentialsCommand
 {
+	@Override
+	public void run(final IUser user, final String commandLabel, final String[] args) throws Exception
+	{
+		if (args.length < 1)
+		{
+			throw new NotEnoughArgumentsException();
+		}
+		ess.broadcastMessage(null, _("§r§6[§4Broadcast§6]§a {0}", FormatUtil.replaceFormat(getFinalArg(args, 0)), user.getPlayer().getDisplayName()));
+	}
+
 	@Override
 	public void run(final CommandSender sender, final String commandLabel, final String[] args) throws Exception
 	{
@@ -14,6 +28,6 @@ public class Commandbroadcast extends EssentialsCommand
 		{
 			throw new NotEnoughArgumentsException();
 		}
-		ess.broadcastMessage(null, _("§r§6[§4Broadcast§6]§a {0}", FormatUtil.replaceFormat(getFinalArg(args, 0))));
+		ess.broadcastMessage(null, _("§r§6[§4Broadcast§6]§a {0}", FormatUtil.replaceFormat(getFinalArg(args, 0)), sender.getName()));
 	}
 }
