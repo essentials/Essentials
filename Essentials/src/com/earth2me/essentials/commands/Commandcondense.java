028453c854ad348af4c285dd81a012bfdd86eece
Faris Rehman <farisrehman@hotmail.co.uk>
2014-05-16 19:51:19 +0100

    Tested and working code
    Testing and working code pushed.

--- Essentials/src/com/earth2me/essentials/commands/Commandcondense.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandcondense.java
@@ -28,17 +28,17 @@ public class Commandcondense extends EssentialsCommand {
 	
 	private Map<ItemStack, SimpleRecipe> condenseList = new HashMap<ItemStack, SimpleRecipe>();
 
+	@SuppressWarnings("deprecation")
 	@Override
-	public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception
-	{
+	public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception {
+		Player player = user.getBase();
 		List<ItemStack> is = new ArrayList<ItemStack>();
-		boolean validateReverse;
 
+		boolean validateReverse = false;
 		if (args.length > 0) {
-			is = ess.getItemDb().getMatching(user, args);
-			validateReverse = false;
+			is = this.ess.getItemDb().getMatching(user, args);
 		} else {
-			for (ItemStack stack : user.getBase().getInventory().getContents()) {
+			for (ItemStack stack : player.getInventory().getContents()) {
 				if (stack == null || stack.getType() == Material.AIR) {
 					continue;
 				}
@@ -50,40 +50,30 @@ public class Commandcondense extends EssentialsCommand {
 		for (final ItemStack itemStack : is) {
 			condenseStack(user, itemStack, validateReverse);
 		}
-		user.getBase().updateInventory();
+		player.updateInventory();
 	}
 
 	private void condenseStack(final User user, final ItemStack stack, final boolean validateReverse) throws ChargeException, MaxMoneyException {
 		final SimpleRecipe condenseType = getCondenseType(stack);
-		if (condenseType != null)
-		{
+		if (condenseType != null) {
 			final ItemStack input = condenseType.getInput();
 			final ItemStack result = condenseType.getResult();
 
-			if (validateReverse)
-			{
+			if (validateReverse) {
 				boolean pass = false;
-				for (Recipe revRecipe : ess.getServer().getRecipesFor(input))
-				{
-
-					if (getStackOnRecipeMatch(revRecipe, result) != null)
-					{
+				for (Recipe revRecipe : this.ess.getServer().getRecipesFor(input)) {
+					if (getStackOnRecipeMatch(revRecipe, result) != null) {
 						pass = true;
 						break;
 					}
 				}
-				if (!pass)
-				{
-					return;
-				}
+				if (!pass) return;
 			}
 
 			int amount = 0;
 
-			for (final ItemStack contents : user.getBase().getInventory().getContents())
-			{
-				if (contents != null && contents.isSimilar(stack))
-				{
+			for (final ItemStack contents : user.getBase().getInventory().getContents()) {
+				if (contents != null && contents.isSimilar(stack)) {
 					amount += contents.getAmount();
 				}
 			}
@@ -91,113 +81,87 @@ public class Commandcondense extends EssentialsCommand {
 			int output = ((amount / input.getAmount()) * result.getAmount());
 			amount -= amount % input.getAmount();
 
-			if (amount > 0)
-			{
+			if (amount > 0) {
 				input.setAmount(amount);
 				result.setAmount(output);
-				final Trade remove = new Trade(input, ess);
-				final Trade add = new Trade(result, ess);
-				remove.charge(user);
-				add.pay(user, OverflowType.DROP);
+				new Trade(input, this.ess).charge(user);
+				new Trade(result, this.ess).pay(user, OverflowType.DROP);
 			}
 		}
 	}
 
-	private SimpleRecipe getCondenseType(final ItemStack stack)
-	{
-		if (condenseList.containsKey(stack))
-		{
-			return condenseList.get(stack);
+	private SimpleRecipe getCondenseType(final ItemStack stack) {
+		if (this.condenseList.containsKey(stack)) {
+			return this.condenseList.get(stack);
 		}
 
-		final Iterator<Recipe> intr = ess.getServer().recipeIterator();
-		while (intr.hasNext())
-		{
+		final Iterator<Recipe> intr = this.ess.getServer().recipeIterator();
+		while (intr.hasNext()) {
 			final Recipe recipe = intr.next();
 			final Collection<ItemStack> recipeItems = getStackOnRecipeMatch(recipe, stack);
 
-			if (recipeItems != null && (recipeItems.size() == 4 || recipeItems.size() == 9)
-				&& (recipeItems.size() > recipe.getResult().getAmount()))
-			{
+			if (recipeItems != null && (recipeItems.size() == 4 || recipeItems.size() == 9) && (recipeItems.size() > recipe.getResult().getAmount())) {
 				final ItemStack input = stack.clone();
 				input.setAmount(recipeItems.size());
-				final SimpleRecipe newRecipe = new SimpleRecipe(recipe.getResult(), input);
-				condenseList.put(stack, newRecipe);
+				SimpleRecipe newRecipe = new SimpleRecipe(recipe.getResult(), input);
+				this.condenseList.put(stack, newRecipe);
 				return newRecipe;
 			}
 		}
-
-		condenseList.put(stack, null);
+		this.condenseList.put(stack, null);
 		return null;
 	}
 
-	private Collection<ItemStack> getStackOnRecipeMatch(final Recipe recipe, final ItemStack stack)
-	{
+	private Collection<ItemStack> getStackOnRecipeMatch(final Recipe recipe, final ItemStack stack) {
 		final Collection<ItemStack> inputList;
 
-		if (recipe instanceof ShapedRecipe)
-		{
-			ShapedRecipe sRecipe = (ShapedRecipe)recipe;
+		if (recipe instanceof ShapedRecipe) {
+			ShapedRecipe sRecipe = (ShapedRecipe) recipe;
 			inputList = sRecipe.getIngredientMap().values();
-		}
-		else if (recipe instanceof ShapelessRecipe)
-		{
-			ShapelessRecipe slRecipe = (ShapelessRecipe)recipe;
+		} else if (recipe instanceof ShapelessRecipe) {
+			ShapelessRecipe slRecipe = (ShapelessRecipe) recipe;
 			inputList = slRecipe.getIngredientList();
-		}
-		else
-		{
+		} else {
 			return null;
 		}
 
 		boolean match = true;
 		Iterator<ItemStack> iter = inputList.iterator();
-		while (iter.hasNext())
-		{
+		while (iter.hasNext()) {
 			ItemStack inputSlot = iter.next();
-			if (inputSlot == null)
-			{
+			if (inputSlot == null) {
 				iter.remove();
 				continue;
 			}
 
-			if (inputSlot.getDurability() == Short.MAX_VALUE)
-			{
-				inputSlot.setDurability((short)0);
+			if (inputSlot.getDurability() == Short.MAX_VALUE) {
+				inputSlot.setDurability((short) 0);
 			}
-			if (!inputSlot.isSimilar(stack))
-			{
+			if (!inputSlot.isSimilar(stack)) {
 				match = false;
 			}
 		}
 
-		if (match)
-		{
-			return inputList;
-		}
-		return null;
+		if (match) return inputList;
+		else return null;
 	}
-
 
 	private class SimpleRecipe implements Recipe {
 		private ItemStack result;
 		private ItemStack input;
 
-		private SimpleRecipe(ItemStack result, ItemStack input)
-		{
+		private SimpleRecipe(ItemStack result, ItemStack input) {
 			this.result = result;
 			this.input = input;
 		}
 
 		@Override
-		public ItemStack getResult()
-		{
-			return result.clone();
+		public ItemStack getResult() {
+			return this.result.clone();
 		}
 
-		public ItemStack getInput()
-		{
-			return input.clone();
+		public ItemStack getInput() {
+			return this.input.clone();
 		}
 	}
 }
