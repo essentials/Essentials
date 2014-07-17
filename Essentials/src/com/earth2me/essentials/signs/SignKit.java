1fb3517c3fdb843256e3bc3e46110d6333aa5928
Necrodoom <doomed.war@gmail.com>
2014-07-17 13:50:00 +0300

    Future proof [kit] sign

--- Essentials/src/com/earth2me/essentials/signs/SignKit.java
+++ Essentials/src/com/earth2me/essentials/signs/SignKit.java
@@ -25,7 +25,7 @@ public class SignKit extends EssentialsSign
 
 		if (kitName.isEmpty())
 		{
-			sign.setLine(1, "§dKit name!");
+			sign.setLine(1, ChatColor.LIGHT_PURPLE + "Kit name!");
 			return false;
 		}
 		else
@@ -41,7 +41,7 @@ public class SignKit extends EssentialsSign
 			final String group = sign.getLine(2);
 			if ("Everyone".equalsIgnoreCase(group) || "Everybody".equalsIgnoreCase(group))
 			{
-				sign.setLine(2, "§2Everyone");
+				sign.setLine(2, ChatColor.DARK_GREEN + "Everyone");
 			}
 			return true;
 		}
@@ -52,7 +52,7 @@ public class SignKit extends EssentialsSign
 	{
 		final String kitName = sign.getLine(1).toLowerCase(Locale.ENGLISH).trim();
 		final String group = sign.getLine(2).trim();
-		if ((!group.isEmpty() && ("§2Everyone".equals(group) || player.inGroup(group)))
+		if ((!group.isEmpty() && (ChatColor.DARK_GREEN + "Everyone".equals(group) || player.inGroup(group)))
 			|| (group.isEmpty() && (player.isAuthorized("essentials.kits." + kitName))))
 		{
 			final Trade charge = getTrade(sign, 3, ess);
