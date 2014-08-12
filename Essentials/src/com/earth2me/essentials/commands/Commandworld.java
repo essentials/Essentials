1b21939126a8ff3ed34848fb068512e360ee9867
Necrodoom <doomed.war@gmail.com>
2014-08-12 17:12:22 +0300

    Add better multiworld and spawn support for /world

--- Essentials/src/com/earth2me/essentials/commands/Commandworld.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandworld.java
@@ -24,27 +24,31 @@ public class Commandworld extends EssentialsCommand
 
 		if (args.length < 1)
 		{
-			World nether = null;
+			World nether = ess.getWorld(user.getWorld().getName() + "_nether");
 
-			final List<World> worlds = server.getWorlds();
+            final List<World> worlds = server.getWorlds();
 
-			for (World world2 : worlds)
-			{
-				if (world2.getEnvironment() == World.Environment.NETHER)
-				{
-					nether = world2;
-					break;
-				}
-			}
-			if (nether == null)
-			{
-				return;
-			}
-			world = user.getWorld() == nether ? worlds.get(0) : nether;
+            if (nether == null)
+            {
+
+
+                for (World world2 : worlds)
+                {
+                    if (world2.getEnvironment() == World.Environment.NETHER)
+                    {
+                        nether = world2;
+                        break;
+                    }
+                }
+            }
+            if (nether == null) {
+                throw new NotEnoughArgumentsException();
+            }
+            world = user.getWorld() == nether ? worlds.get(0) : nether;
 		}
 		else
 		{
-			world = ess.getWorld(getFinalArg(args, 0));
+			world = ess.getWorld(condenseArgs(args));
 			if (world == null)
 			{
 				user.sendMessage(tl("invalidWorld"));
@@ -59,26 +63,56 @@ public class Commandworld extends EssentialsCommand
 			throw new Exception(tl("noPerm", "essentials.worlds." + world.getName()));
 		}
 
-		double factor;
-		if (user.getWorld().getEnvironment() == World.Environment.NETHER && world.getEnvironment() == World.Environment.NORMAL)
-		{
-			factor = 8.0;
-		}
-		else if (user.getWorld().getEnvironment() == World.Environment.NORMAL && world.getEnvironment() == World.Environment.NETHER)
-		{
-			factor = 1.0 / 8.0;
-		}
-		else
-		{
-			factor = 1.0;
-		}
+        Location target;
 
-		final Location loc = user.getLocation();
-		final Location target = new Location(world, loc.getBlockX() * factor + .5, loc.getBlockY(), loc.getBlockZ() * factor + .5);
+        if (args.length > 1 && args[args.length - 1].equals("spawn"))
+        {
+            target = world.getSpawnLocation();
+        }
+        else
+        {
+            double factor;
+            if (user.getWorld().getEnvironment() == World.Environment.NETHER && world.getEnvironment() == World.Environment.NORMAL)
+            {
+                factor = 8.0;
+            } else if (user.getWorld().getEnvironment() == World.Environment.NORMAL && world.getEnvironment() == World.Environment.NETHER)
+            {
+                factor = 1.0 / 8.0;
+            } else
+            {
+                factor = 1.0;
+            }
 
-		final Trade charge = new Trade(this.getName(), ess);
-		charge.isAffordableFor(user);
-		user.getTeleport().teleport(target, charge, TeleportCause.COMMAND);
-		throw new NoChargeException();
+            Location loc = user.getLocation();
+            target = new Location(world, loc.getBlockX() * factor + .5, loc.getBlockY(), loc.getBlockZ() * factor + .5);
+
+        }
+        final Trade charge = new Trade(this.getName(), ess);
+        charge.isAffordableFor(user);
+        user.getTeleport().teleport(target, charge, TeleportCause.COMMAND);
+        throw new NoChargeException();
 	}
+
+    String condenseArgs(String[] args)
+    {
+        if (args.length == 1)
+        {
+            return args[0];
+        }
+        final StringBuilder bldr = new StringBuilder();
+        for (int i = 0; i < args.length - 1; i++)
+        {
+            if (i != 0)
+            {
+                bldr.append(" ");
+            }
+            bldr.append(args[i]);
+        }
+
+        if (!args[args.length - 1].equals("spawn"))
+        {
+            bldr.append(" ").append(args[args.length - 1]);
+        }
+        return bldr.toString();
+    }
 }
