89f69b8a047e06f4b4f176232667a2f935eb913a
Matthew Miller (Me4502) <mnmiller1@hotmail.com>
2016-02-27 23:27:19 +1000

    Update for latest SpongeAPI & ModularFramework.

--- src/main/java/org/mcess/essentials/Essentials.java
+++ src/main/java/org/mcess/essentials/Essentials.java
@@ -3,10 +3,11 @@ package org.mcess.essentials;
 import com.google.inject.Inject;
 import com.me4502.modularframework.ModuleController;
 import com.me4502.modularframework.ShadedModularFramework;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.service.config.DefaultConfig;
 
 import java.io.File;
 
@@ -19,14 +20,12 @@ public class Essentials {
     @DefaultConfig(sharedRoot = false)
     private File mainConfig;
 
-    private File configurationDirectory;
-
     @Listener
     public void onInitialize(GameStartedServerEvent event) {
 
-        moduleController = ShadedModularFramework.registerModuleController(this, event.getGame());
+        moduleController = ShadedModularFramework.registerModuleController(this, Sponge.getGame());
 
-        configurationDirectory = new File(mainConfig.getParent(), "modules");
+        File configurationDirectory = new File(mainConfig.getParent(), "modules");
         configurationDirectory.mkdir();
         moduleController.setConfigurationDirectory(configurationDirectory);
 
--- src/main/java/org/mcess/essentials/modules/Teleport.java
+++ src/main/java/org/mcess/essentials/modules/Teleport.java
@@ -1,30 +1,30 @@
 package org.mcess.essentials.modules;
 
 import com.me4502.modularframework.module.Module;
-import org.spongepowered.api.text.Texts;
-import org.spongepowered.api.util.command.CommandException;
-import org.spongepowered.api.util.command.CommandResult;
-import org.spongepowered.api.util.command.CommandSource;
-import org.spongepowered.api.util.command.args.CommandContext;
-import org.spongepowered.api.util.command.spec.CommandExecutor;
-import org.spongepowered.api.util.command.spec.CommandSpec;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
 
 @Module(moduleName = "Teleport", onEnable = "onInitialize")
 public class Teleport {
 
     public void onInitialize() {
         CommandSpec myCommandSpec = CommandSpec.builder()
-                .description(Texts.of("Teleport to a player"))
+                .description(Text.of("Teleport to a player"))
                 .permission("essentials.teleport")
                 .executor(new TeleportCommand())
                 .build();
     }
 
-    private class TeleportCommand implements CommandExecutor {
+    private static class TeleportCommand implements CommandExecutor {
 
         @Override
         public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-            return null;
+            return CommandResult.success();
         }
     }
 }
